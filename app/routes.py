8a8941bd98c286b4605534c47ca48319d4474396
Steven <xpogosteven@gmail.com>
2019-05-06 20:22:18 -0400

    Added Login form and other changes related to login

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,35 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/home')
+@login_required
 def home():
-    return render_template('base.html', title='Home')
+    return render_template('home.html', title='Home')
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form=LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid Username or Password")
+            redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for("home"))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        redirect(next_page)
+    return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
